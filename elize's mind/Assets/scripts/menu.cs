8a27c1643ececf0c84eedeaeab05e504b6cd6691
Giovanni Florio Sannazzaro <giovannifloriosannazzarro@gmail.com>
2026-03-04 23:09:22 -0300

    Documentação
    documentei o meu codigo

--- elize's mind/Assets/scripts/menu.cs
+++ elize's mind/Assets/scripts/menu.cs
@@ -1,32 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement; //importamos a funcionalidade de cenas para conseguir mexer em cenas
 
 public class menu : MonoBehaviour
 {
 
-    public cair objetoQueVaiCair;
+    public cair objetoQueVaiCair; //Crio uma variavel publica com o nome cair, onde eu arrasto o objeto que vai cair(menu opções)
 
-    public void Jogar()
+    public void Jogar() //Declaro uma função jogar
     {
-        SceneManager.LoadScene("jogo");
+        SceneManager.LoadScene("jogo"); //Quando chamar o função jogar, a cena muda para a do jogo.
     }
 
-    public void DescerBotao()
+    public void DescerBotao() //Declaro uma função chamda descerbotão
     {
-        objetoQueVaiCair.Descer();
+        objetoQueVaiCair.Descer(); //Faço o objeto chamar a função descer(que defini na script cair, nem eu sei como fiz isso)
     }
 
-    public void SubirBotao()
+    public void SubirBotao() //Declaro uma função chamada subirbotão
     {
-        objetoQueVaiCair.Subir();
+        objetoQueVaiCair.Subir(); //Faço chamar a função descer no objetoquevaicair
     }
 
-    public void Sair()
+    public void Sair() //Declaro uma função chamada sair
     {
-        Debug.Log("Fechou");
-        Application.Quit();
+        Application.Quit(); //Fecha o aplicativo
     }
 
 
--- elize's mind/Assets/scripts/mov_player.cs
+++ elize's mind/Assets/scripts/mov_player.cs
@@ -3,24 +3,32 @@ using UnityEngine.InputSystem;
 
 public class mov_player : MonoBehaviour
 {
-    private Vector2 movement;
-    [SerializeField] private float speed = 5f;
-    private Rigidbody2D rb;
+    private Vector2 movement; //Criamos um vector 2(algo que contem o valor X e Y) e damos o nome dele de movement
+    [SerializeField] private float speed = 5f; //Criamos uma variavel de velocidade, onde deixamos como 5(para ela andar 5x mais rapido que o normal)
+                                               //ela é private(pq vi q é bom ser assim) e tem o Serialize Field para consiguir alterir isso no menu da unity
 
-    private void Awake()
+    private Rigidbody2D rb; //Criei uma variavel chamada 'rb', para obter o Rigidbody2D do objeto player, para ser possivel mexer ele
+
+    private void Awake() //O 'awake' serve para ser chamado antes do jogo começar, serve para inicializar variaveis
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); //Aqui iniciamos o componente Rigidbody2D do objeto player, para ser possivel mexer ele
 
     }
 
-    private void OnMovement(InputValue value)
+    private void OnMovement(InputValue value) //Quando o jogador se mexer, vai mandar o valor 'InputValue' (do input system*) para a variavel 'value'
     {
-        movement = value.Get<Vector2>();
+        movement = value.Get<Vector2>(); //Aqui armazenamos o valor do movimento do jogador(value.get) e transformamos ele em um vector2(X,Y) armazenado na variavel movement
 
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate() //FixedUptade é chamado muito frequentemente, e onde acontece o movimento.
     {
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime); //Aqui movemos o jogador no rigidbody(rb) usando a função movePosition, onde usamos a
+                                                                               //posição atual dele (rb.position) + o valor do movimento(que recebemos do input system, sendo
+                                                                               //x +- (y tbm) e quando na diagonal é 0.7 em ambas as direções, multiplicando pela velocidade
+                                                                               //e multiplicamos pelo Time.fixedDeltaTime para q o movimento aconteça a cada um periodo de tempo,
+                                                                               //não a cada frame(pois jogadores podem jogar em framerate diferente.
+
+
     }
 }
